fdc3fac7435c8213ab5b2fbbbba9fe081b10209e
Nuno Maduro <enunomaduro@gmail.com>
2021-07-13 13:36:18 +0100

    Adds `server:list` command

---
+++ app/Commands/ServerListCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Commands;
+
+class ServerListCommand extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'server:list';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'List the servers';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->table([
+            'ID', 'Name', 'IP Address',
+        ], collect($this->forge->servers())->map(function ($server) {
+            return [
+                $server->id,
+                $server->name,
+                $server->ipAddress,
+            ];
+        })->all());
+    }
+}
---
+++ tests/Feature/ServerListCommandTest.php
@@ -0,0 +1,14 @@
+<?php
+
+it('displays the list of servers', function () {
+    $this->client->shouldReceive('servers')->andReturn([
+        (object) ['id' => 1, 'name' => 'production', 'ipAddress' => '123.456.789.000'],
+        (object) ['id' => 2, 'name' => 'staging', 'ipAddress' => '789.456.123.111'],
+    ]);
+
+    $this->artisan('server:list')
+        ->expectsTable(['ID', 'Name', 'IP Address'], [
+            ['id' => 1, 'name' => 'production', 'ipAddress' => '123.456.789.000'],
+            ['id' => 2, 'name' => 'staging', 'ipAddress' => '789.456.123.111'],
+        ]);
+});
