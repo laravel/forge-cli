0cd4b33aaaa59e1db6c6df29ce5bc38b8c66e4b4
Nuno Maduro <enunomaduro@gmail.com>
2021-07-15 18:22:08 +0100

    Adds `php:status` command

---
+++ app/Commands/PhpStatusCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Commands;
+
+use App\Exceptions\LogicException;
+
+class PhpStatusCommand extends Command
+{
+    use Concerns\InteractsWithPhp;
+
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'php:status {--type=}';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Get the current status of php';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->ensurePhpExists();
+
+        $server = $this->currentServer();
+
+        $version = $this->option('type');
+        $versions = ['5.6', '7.0', '7.1', '7.2', '7.3', '7.4', '8.0'];
+
+        if (! is_null($version) && ! in_array($version, $versions)) {
+            throw new LogicException('PHP version needs to be one of those values: '.implode(', ', $versions).'.');
+        }
+
+        $version = $version ?: number_format(substr($server->phpVersion, -2) / 10, 1, '.', '');
+
+        $status = $this->serviceStatus($server, 'php'.$version.'-fpm');
+
+        $this->info('PHP '.$version.' service is '.$status.'.');
+    }
+}
---
+++ tests/Feature/PhpStatusCommandTest.php
@@ -0,0 +1,37 @@
+<?php
+
+it('can display the php status running', function () {
+    $this->client->shouldReceive('server')->andReturn(
+        (object) ['id' => 1, 'name' => 'production', 'ipAddress' => '123.456.789.222', 'phpVersion' => 'php56'],
+    );
+
+    $this->shell->shouldReceive('exec')->andReturn([0]);
+
+    $this->artisan('php:status')->expectsOutput('PHP 5.6 service is [running].');
+});
+
+it('can display the php status as inactive', function () {
+    $this->client->shouldReceive('server')->andReturn(
+        (object) ['id' => 1, 'name' => 'production', 'ipAddress' => '123.456.789.222', 'phpVersion' => 'php80'],
+    );
+
+    $this->shell->shouldReceive('exec')->andReturn([3]);
+
+    $this->artisan('php:status')->expectsOutput('PHP 8.0 service is [inactive].');
+});
+
+it('can not display the status when php is incorrect', function () {
+    $this->client->shouldReceive('server')->andReturn(
+        (object) ['id' => 1, 'name' => 'production', 'phpVersion' => 'php56'],
+    );
+
+    $this->artisan('php:status', ['--type' => '2.0']);
+})->throws('PHP version needs to be one of those values: 5.6, 7.0, 7.1, 7.2, 7.3, 7.4, 8.0.');
+
+it('can not display the status when there is no php', function () {
+    $this->client->shouldReceive('server')->andReturn(
+        (object) ['id' => 1, 'name' => 'production', 'phpVersion' => null],
+    );
+
+    $this->artisan('php:status');
+})->throws('PHP is not installed in this server.');
