ff28c09b6b316046488b499aeec559f3bc5a85cd
Nuno Maduro <enunomaduro@gmail.com>
2021-07-15 18:21:47 +0100

    Fixes `nginx:status` test

--- tests/Feature/NginxStatusCommandTest.php
+++ tests/Feature/NginxStatusCommandTest.php
@@ -1,13 +1,13 @@
 <?php
 
-it('can display the nginx status active', function () {
+it('can display the nginx status running', function () {
     $this->client->shouldReceive('server')->andReturn(
         (object) ['id' => 1, 'name' => 'production', 'ipAddress' => '123.456.789.222'],
     );
 
     $this->shell->shouldReceive('exec')->andReturn([0]);
 
-    $this->artisan('nginx:status')->expectsOutput('Nginx is [active].');
+    $this->artisan('nginx:status')->expectsOutput('Nginx service is [running].');
 });
 
 it('can display the nginx status as inactive', function () {
@@ -17,5 +17,5 @@ it('can display the nginx status as inactive', function () {
 
     $this->shell->shouldReceive('exec')->andReturn([3]);
 
-    $this->artisan('nginx:status')->expectsOutput('Nginx is [inactive].');
+    $this->artisan('nginx:status')->expectsOutput('Nginx service is [inactive].');
 });
