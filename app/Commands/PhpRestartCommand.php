24d1b71d762e4874821829453f2c7fc539950e0a
Nuno Maduro <enunomaduro@gmail.com>
2021-07-15 19:30:26 +0100

    Adds `php:restart` command

---
+++ app/Commands/PhpRestartCommand.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Commands;
+
+use Laravel\Forge\Exceptions\ValidationException;
+
+class PhpRestartCommand extends Command
+{
+    use Concerns\InteractsWithPhp;
+
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'php:restart {--type=}';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Restart PHP';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->ensurePhpExists();
+
+        $server = $this->currentServer();
+
+        $version = $this->option('type');
+        $versions = ['5.6', '7.0', '7.1', '7.2', '7.3', '7.4', '8.0'];
+
+        if (! is_null($version) && ! in_array($version, $versions)) {
+            abort(1, 'PHP version needs to be one of those values: '.implode(', ', $versions).'.');
+        }
+
+        $version = $version ?: number_format(substr($server->phpVersion, -2) / 10, 1, '.', '');
+
+        if ($this->restartPhp($server->id, $version)) {
+            $this->info('PHP '.$version.' restart initiated successfully.');
+        }
+    }
+
+    /**
+     * Restarts PHP service.
+     *
+     * @param  string|int  $serverId
+     * @param  string  $version
+     * @return bool
+     */
+    public function restartPhp($serverId, $version)
+    {
+        if ($restarting = $this->confirm('While the <comment>[PHP '.$version.']</comment> service restarts, sites may become unavailable. Wish to proceed?')) {
+            try {
+                $this->forge->rebootPHP($serverId, [
+                    'version' => 'php'.str_replace('.', '', $version),
+                ]);
+            } catch (ValidationException $e) {
+                abort(1, collect($e->errors())->first());
+            }
+        }
+
+        return $restarting;
+    }
+}
---
+++ tests/Feature/PhpRestartCommandTest.php
@@ -0,0 +1,46 @@
+<?php
+
+it('can restart php', function () {
+    $this->client->shouldReceive('server')->andReturn(
+        (object) ['id' => 1, 'name' => 'production', 'phpVersion' => 'php80'],
+    );
+
+    $this->client->shouldReceive('rebootPhp');
+
+    $this->artisan('php:restart')
+        ->expectsConfirmation(
+            'While the <comment>[PHP 8.0]</comment> service restarts, sites may become unavailable. Wish to proceed?',
+            'yes',
+        )->expectsOutput('PHP 8.0 restart initiated successfully.');
+});
+
+it('can restart a specific php version', function () {
+    $this->client->shouldReceive('server')->andReturn(
+        (object) ['id' => 1, 'name' => 'production', 'phpVersion' => 'php56'],
+    );
+
+    $this->client->shouldReceive('rebootPhp');
+
+    $this->artisan('php:restart', [
+        '--type' => '7.4',
+    ])->expectsConfirmation(
+        'While the <comment>[PHP 7.4]</comment> service restarts, sites may become unavailable. Wish to proceed?',
+        'yes',
+    )->expectsOutput('PHP 7.4 restart initiated successfully.');
+});
+
+it('can restart php when php version is incorrect', function () {
+    $this->client->shouldReceive('server')->andReturn(
+        (object) ['id' => 1, 'name' => 'production', 'phpVersion' => 'php80'],
+    );
+
+    $this->artisan('php:restart', ['--type' => '2.0']);
+})->throws('PHP version needs to be one of those values: 5.6, 7.0, 7.1, 7.2, 7.3, 7.4, 8.0.');
+
+it('can not restart php when there is no php', function () {
+    $this->client->shouldReceive('server')->andReturn(
+        (object) ['id' => 1, 'name' => 'production', 'phpVersion' => null],
+    );
+
+    $this->artisan('php:status');
+})->throws('PHP is not installed in this server.');
