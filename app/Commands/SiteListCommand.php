454374d2e7e95e845860cebe6cd0934d5cb1b21d
Nuno Maduro <enunomaduro@gmail.com>
2021-07-16 00:04:41 +0100

    Adds `site:list` command

---
+++ app/Commands/SiteListCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Commands;
+
+use App\Support\PhpVersion;
+
+class SiteListCommand extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'site:list';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'List the sites';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $sites = $this->forge->sites(
+            $this->currentServer()->id
+        );
+
+        $this->table([
+            'ID', 'Name', 'PHP',
+        ], collect($sites)->map(function ($site) {
+            return [
+                $site->id,
+                $site->name,
+                $site->phpVersion ? PhpVersion::of($site->phpVersion)->release() : 'None',
+            ];
+        })->all());
+    }
+}
---
+++ tests/Feature/SiteListCommandTest.php
@@ -0,0 +1,18 @@
+<?php
+
+it('can display the list of sites', function () {
+    $this->client->shouldReceive('server')->andReturn(
+        (object) ['id' => 1],
+    );
+
+    $this->client->shouldReceive('sites')->andReturn([
+        (object) ['id' => 1, 'name' => 'production.com', 'phpVersion' => 'php56'],
+        (object) ['id' => 2, 'name' => 'staging.com', 'phpVersion' => null],
+    ]);
+
+    $this->artisan('site:list')
+        ->expectsTable(['ID', 'Name', 'PHP'], [
+            ['id' => 1, 'name' => 'production.com', 'phpVersion' => '5.6'],
+            ['id' => 2, 'name' => 'staging.com', 'phpVersion' => 'None'],
+        ]);
+});
