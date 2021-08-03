aced4f9d7f50fa22e683ce49111d56d7e14ac3ab
Nuno Maduro <enunomaduro@gmail.com>
2021-08-03 15:08:58 +0100

    Displays the issue create link when errors happen

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Exceptions;
+
+use App\Support\Panic;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use LaravelZero\Framework\Exceptions\ConsoleException;
+use Throwable;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        ConsoleException::class,
+    ];
+
+    /**
+     * Register the exception handling callbacks for the application.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->reportable(function (Throwable $e) {
+            if (app()->isProduction()) {
+                Panic::abort($e);
+            }
+
+            return true;
+        });
+    }
+}
--- app/Support/Panic.php
+++ app/Support/Panic.php
@@ -2,22 +2,31 @@
 
 namespace App\Support;
 
+use Throwable;
+
 class Panic
 {
     /**
      * Abort in unexpected issues.
      *
-     * @param  string  $message
+     * @param  \Throwable|string  $message
      * @return never
      */
     public static function abort($message)
     {
+        if ($message instanceof Throwable) {
+            $message = $message->getMessage();
+        }
+
         abort(1, sprintf(<<<'EOF'
-            An unexpected error occured. Please report this issue here: https://github.com/laravel/forge-cli.
-            - Issue: %s.
-            - PHP version: %s.
-            - Operating system: %s.
+            An unexpected error occured. Please report this issue here:
+            https://github.com/laravel/forge-cli/issues/new/choose
+
+            - Forge CLI Version: %s
+            - PHP Version: %s
+            - Operating System: %s
+            - Error Message: %s.
             EOF
-        , $message, phpversion(), PHP_OS));
+        , config('app.version'), phpversion(), PHP_OS, $message));
     }
 }
