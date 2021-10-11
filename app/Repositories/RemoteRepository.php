d6babf8f8863f5b5184f7a7dba9578cffbbc8fc4
geisi <tim@partysturmevents.de>
2021-10-11 18:58:46 +0200

    remove phpstorm style changes

--- app/Repositories/RemoteRepository.php
+++ app/Repositories/RemoteRepository.php
@@ -2,7 +2,6 @@
 
 namespace App\Repositories;
 
-use App\Support\Boolean;
 use Illuminate\Support\Arr;
 use Symfony\Component\Process\Process;
 
@@ -59,70 +58,24 @@ class RemoteRepository
     /**
      * Execute a command against the shell, and displays the output.
      *
-     * @param string|null $command
+     * @param  string|null  $command
      * @return int
      */
     public function passthru($command = null)
     {
         $this->ensureSshIsConfigured();
 
-        passthru($this->ssh('"' . $command . '"'), $exitCode);
+        passthru($this->ssh('"'.$command.'"'), $exitCode);
 
-        return (int)$exitCode;
-    }
-
-    /**
-     * Ensure user can connect to current server.
-     *
-     * @return void
-     */
-    public function ensureSshIsConfigured()
-    {
-        once(function () {
-            abort_if(is_null($this->serverResolver), 1, 'Current server unresolvable.');
-
-            if (is_null($this->server)) {
-                $this->server = call_user_func($this->serverResolver);
-            }
-
-            $this->sanitizableOutput = exec($this->ssh('-t exit 0'), $_, $exitCode);
-
-            abort_if($exitCode > 0, 1, 'Unable to connect to remote server. Maybe run [ssh:configure] to configure an SSH Key?');
-        });
-    }
-
-    /**
-     * Returns the "ssh" sheel command to be run.
-     *
-     * @param string $command |null
-     * @return string
-     */
-    protected function ssh($command = null)
-    {
-        $options = collect([
-            'ConnectTimeout' => 5,
-            'ControlMaster' => 'auto',
-            'ControlPersist' => 100,
-            'ControlPath' => $this->socketsPath . '/%h-%p-%r',
-            'LogLevel' => 'QUIET',
-        ])->map(function ($value, $option) {
-            return "-o $option=$value";
-        })->values()->implode(' ');
-
-        return trim(sprintf(
-            'ssh %s -t forge@%s %s',
-            $options,
-            Boolean::fromValue($this->config->get('ssh_private_ip_access', false)) ? $this->server->privateIpAddress : $this->server->ipAddress,
-            $command,
-        ));
+        return (int) $exitCode;
     }
 
     /**
      * Tails the given file, and runs the given callback on each output.
      *
-     * @param array|string $files
-     * @param callable $callback
-     * @param array $options
+     * @param  array|string  $files
+     * @param  callable  $callback
+     * @param  array  $options
      * @return array
      */
     public function tail($files, $callback, $options = [])
@@ -135,7 +88,7 @@ class RemoteRepository
 
         $command = collect(explode(' ', $this->ssh()))->merge(['tail', '-n', '500'])
             ->merge($options)
-            ->push('$(ls -1td ' . $files->implode(' ') . ' 2>/dev/null | head -n1)')
+            ->push('$(ls -1td '.$files->implode(' ').' 2>/dev/null | head -n1)')
             ->filter()
             ->values()
             ->all();
@@ -162,30 +115,13 @@ class RemoteRepository
             ? 0 // Control + C
             : $process->getExitCode();
 
-        return [(int)$exitCode, $output];
-    }
-
-    /**
-     * Ensures the given files are tailable.
-     *
-     * @param \Illuminate\Support\Collection $files
-     * @return void
-     */
-    protected function ensuresFilesAreTailable($files)
-    {
-        $files->reject(function ($file) {
-            [$exitCode] = $this->exec('ls -1td ' . $file);
-
-            return $exitCode > 0;
-        })->whenEmpty(function () {
-            abort(1, 'The requested logs could not be found or they are empty.');
-        });
+        return [(int) $exitCode, $output];
     }
 
     /**
      * Execute a command against the shell, and returns the output.
      *
-     * @param string $command
+     * @param  string  $command
      * @return array
      */
     public function exec($command)
@@ -198,17 +134,80 @@ class RemoteRepository
             unset($output[0]);
         }
 
-        return [(int)$exitCode, array_values($output)];
+        return [(int) $exitCode, array_values($output)];
     }
 
     /**
      * Sets the current server.
      *
-     * @param callable $resolver
+     * @param  callable  $resolver
      * @return void
      */
     public function resolveServerUsing($resolver)
     {
         $this->serverResolver = $resolver;
+    }
+
+    /**
+     * Ensure user can connect to current server.
+     *
+     * @return void
+     */
+    public function ensureSshIsConfigured()
+    {
+        once(function () {
+            abort_if(is_null($this->serverResolver), 1, 'Current server unresolvable.');
+
+            if (is_null($this->server)) {
+                $this->server = call_user_func($this->serverResolver);
+            }
+
+            $this->sanitizableOutput = exec($this->ssh('-t exit 0'), $_, $exitCode);
+
+            abort_if($exitCode > 0, 1, 'Unable to connect to remote server. Maybe run [ssh:configure] to configure an SSH Key?');
+        });
+    }
+
+    /**
+     * Ensures the given files are tailable.
+     *
+     * @param  \Illuminate\Support\Collection  $files
+     * @return void
+     */
+    protected function ensuresFilesAreTailable($files)
+    {
+        $files->reject(function ($file) {
+            [$exitCode] = $this->exec('ls -1td '.$file);
+
+            return $exitCode > 0;
+        })->whenEmpty(function () {
+            abort(1, 'The requested logs could not be found or they are empty.');
+        });
+    }
+
+    /**
+     * Returns the "ssh" sheel command to be run.
+     *
+     * @param  string  $command|null
+     * @return string
+     */
+    protected function ssh($command = null)
+    {
+        $options = collect([
+            'ConnectTimeout' => 5,
+            'ControlMaster' => 'auto',
+            'ControlPersist' => 100,
+            'ControlPath' => $this->socketsPath.'/%h-%p-%r',
+            'LogLevel' => 'QUIET',
+        ])->map(function ($value, $option) {
+            return "-o $option=$value";
+        })->values()->implode(' ');
+
+        return trim(sprintf(
+            'ssh %s -t forge@%s %s',
+            $options,
+            $this->server->ipAddress,
+            $command,
+        ));
     }
 }
