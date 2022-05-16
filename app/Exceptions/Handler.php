08a8dd40118be124c7cf8cd540c6ed4febf46a54
Nuno Maduro <enunomaduro@gmail.com>
2022-05-16 09:51:24 +0100

    Fixes builds

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -12,7 +12,7 @@ class Handler extends ExceptionHandler
     /**
      * A list of the exception types that are not reported.
      *
-     * @var array<int, string>
+     * @var array<int, class-string<\Throwable>>
      */
     protected $dontReport = [
         ConsoleException::class,
