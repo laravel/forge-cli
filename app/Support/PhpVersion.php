b48120c7e5392fc6617ec66525ee617ce8707478
Nuno Maduro <enunomaduro@gmail.com>
2023-05-01 11:02:51 +0100

    Update PhpVersion.php

--- app/Support/PhpVersion.php
+++ app/Support/PhpVersion.php
@@ -7,7 +7,7 @@ class PhpVersion
     /**
      * The available PHP versions.
      *
-     * @var array
+     * @var array<int, string>
      */
     const VERSIONS = ['5.6', '7.0', '7.1', '7.2', '7.3', '7.4', '8.0', '8.1', '8.2'];
 
