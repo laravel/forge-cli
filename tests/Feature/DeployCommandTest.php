8a67748e7bf9a13e77d37366b6fa17a53cef9c9a
Josh Salway <josh.salway@gmail.com>
2026-04-07 11:54:59 +1000

    Fix DeployCommandTest missing siteDeploymentOutput mock
    The deploy command calls siteDeploymentOutput() to display deployment
    output, but the tests never mocked this method. This was added to
    the forge-sdk after the tests were written, causing all deploy tests
    to fail with "no expectations were specified".

--- tests/Feature/DeployCommandTest.php
+++ tests/Feature/DeployCommandTest.php
@@ -16,38 +16,20 @@ it('can deploy sites with an menu', function () {
 
     $this->client->shouldReceive('deploySite')->with(1, 1, false)->once()->andReturn(null);
 
-    $this->client->shouldReceive('events')->with(1)->once()->andReturn([
-        (object) ['id' => 3, 'description' => 'Deploying Pushed Code (pestphp.com).'],
-        (object) ['id' => 2, 'description' => 'Deploying Pushed Code (something.com).'],
-        (object) ['id' => 1, 'description' => 'Deploying Pushed Code (pestphp.com).'],
-    ]);
-
     $this->client->shouldReceive('siteDeployments')->with(1, 1)->once()->andReturn([
         ['id' => 3],
     ]);
-
-    $this->remote->shouldReceive('exec')->with(
-        'cat /home/forge/.forge/provision-3.output'
-    )->once()->andReturn([0, [
-        'Installing composer dependencies...',
-    ]]);
-
-    $this->remote->shouldReceive('exec')->with(
-        'cat /home/forge/.forge/provision-3.output'
-    )->once()->andReturn([0, [
-        'Installing composer dependencies...',
-        'Restarting FPM...',
-    ]]);
 
     $this->client->shouldReceive('siteDeployment')->with(1, 1, 3)->once()->andReturn(
         (object) ['id' => 3, 'status' => 'finished', 'started_at' => '2021-07-20 12:50:01', 'ended_at' => '2021-07-20 12:50:09'],
     );
 
+    $this->client->shouldReceive('siteDeploymentOutput')->with(1, 1, 3)->once()->andReturn("Installing composer dependencies...\nRestarting FPM...");
+
     $this->artisan('deploy')
         ->expectsQuestion('<fg=yellow>‣</> <options=bold>Which Site Would You Like To Deploy</>', 1)
         ->expectsOutput('==> Queuing Deployment')
         ->expectsOutput('==> Waiting For Deployment To Start')
-        ->expectsOutput('==> Deploying')
         ->expectsOutput('  ▕ Installing composer dependencies...')
         ->expectsOutput('  ▕ Restarting FPM...')
         ->expectsOutput('==> Site Deployed Successfully. (8s)');
@@ -69,37 +51,19 @@ it('can deploy sites with an option', function () {
 
     $this->client->shouldReceive('deploySite')->with(1, 2, false)->once()->andReturn(null);
 
-    $this->client->shouldReceive('events')->with(1)->once()->andReturn([
-        (object) ['id' => 3, 'description' => 'Deploying Pushed Code (something.com).'],
-        (object) ['id' => 2, 'description' => 'Deploying Pushed Code (something.com).'],
-        (object) ['id' => 1, 'description' => 'Deploying Pushed Code (pestphp.com).'],
-    ]);
-
     $this->client->shouldReceive('siteDeployments')->with(1, 2)->once()->andReturn([
         ['id' => 3],
     ]);
-
-    $this->remote->shouldReceive('exec')->with(
-        'cat /home/forge/.forge/provision-3.output'
-    )->once()->andReturn([0, [
-        'Installing composer dependencies...',
-    ]]);
-
-    $this->remote->shouldReceive('exec')->with(
-        'cat /home/forge/.forge/provision-3.output'
-    )->once()->andReturn([0, [
-        'Installing composer dependencies...',
-        'Restarting FPM...',
-    ]]);
 
     $this->client->shouldReceive('siteDeployment')->with(1, 2, 3)->once()->andReturn(
         (object) ['id' => 3, 'status' => 'finished', 'started_at' => '2021-07-20 12:50:01', 'ended_at' => '2021-07-20 12:50:09'],
     );
 
+    $this->client->shouldReceive('siteDeploymentOutput')->with(1, 2, 3)->once()->andReturn("Installing composer dependencies...\nRestarting FPM...");
+
     $this->artisan('deploy', ['site' => 2])
         ->expectsOutput('==> Queuing Deployment')
         ->expectsOutput('==> Waiting For Deployment To Start')
-        ->expectsOutput('==> Deploying')
         ->expectsOutput('  ▕ Installing composer dependencies...')
         ->expectsOutput('  ▕ Restarting FPM...')
         ->expectsOutput('==> Site Deployed Successfully. (8s)');
@@ -121,37 +85,19 @@ it('can deploy sites when sites use website isolation', function () {
 
     $this->client->shouldReceive('deploySite')->with(1, 2, false)->once()->andReturn(null);
 
-    $this->client->shouldReceive('events')->with(1)->once()->andReturn([
-        (object) ['id' => 3, 'description' => 'Deploying Pushed Code (something.com).'],
-        (object) ['id' => 2, 'description' => 'Deploying Pushed Code (something.com).'],
-        (object) ['id' => 1, 'description' => 'Deploying Pushed Code (pestphp.com).'],
-    ]);
-
     $this->client->shouldReceive('siteDeployments')->with(1, 2)->once()->andReturn([
         ['id' => 3],
     ]);
-
-    $this->remote->shouldReceive('exec')->with(
-        'cat /home/user-in-isolation/.forge/provision-3.output'
-    )->once()->andReturn([0, [
-        'Installing composer dependencies...',
-    ]]);
-
-    $this->remote->shouldReceive('exec')->with(
-        'cat /home/user-in-isolation/.forge/provision-3.output'
-    )->once()->andReturn([0, [
-        'Installing composer dependencies...',
-        'Restarting FPM...',
-    ]]);
 
     $this->client->shouldReceive('siteDeployment')->with(1, 2, 3)->once()->andReturn(
         (object) ['id' => 3, 'status' => 'finished', 'started_at' => '2021-07-20 12:50:01', 'ended_at' => '2021-07-20 12:50:09'],
     );
 
+    $this->client->shouldReceive('siteDeploymentOutput')->with(1, 2, 3)->once()->andReturn("Installing composer dependencies...\nRestarting FPM...");
+
     $this->artisan('deploy', ['site' => 2])
         ->expectsOutput('==> Queuing Deployment')
         ->expectsOutput('==> Waiting For Deployment To Start')
-        ->expectsOutput('==> Deploying')
         ->expectsOutput('  ▕ Installing composer dependencies...')
         ->expectsOutput('  ▕ Restarting FPM...')
         ->expectsOutput('==> Site Deployed Successfully. (8s)');
@@ -190,30 +136,15 @@ it('handles deployment failures', function () {
 
     $this->client->shouldReceive('deploySite')->with(1, 2, false)->once()->andReturn(null);
 
-    $this->client->shouldReceive('events')->with(1)->once()->andReturn([
-        (object) ['id' => 3, 'description' => 'Deploying Pushed Code (something.com).'],
-        (object) ['id' => 2, 'description' => 'Deploying Pushed Code (something.com).'],
-        (object) ['id' => 1, 'description' => 'Deploying Pushed Code (pestphp.com).'],
-    ]);
-
     $this->client->shouldReceive('siteDeployments')->with(1, 2)->once()->andReturn([
         ['id' => 3],
     ]);
 
-    $this->remote->shouldReceive('exec')->with(
-        'cat /home/forge/.forge/provision-3.output'
-    )->once()->andReturn([0, ['Installing composer dependencies...']]);
-
-    $this->remote->shouldReceive('exec')->with(
-        'cat /home/forge/.forge/provision-3.output'
-    )->once()->andReturn([0, [
-        'Installing composer dependencies...',
-        'Restarting FPM failed...',
-    ]]);
-
     $this->client->shouldReceive('siteDeployment')->with(1, 2, 3)->once()->andReturn(
         (object) ['id' => 3, 'status' => 'failed', 'started_at' => '2021-07-20 12:50:01', 'ended_at' => '2021-07-20 12:50:09'],
     );
+
+    $this->client->shouldReceive('siteDeploymentOutput')->with(1, 2, 3)->once()->andReturn("Installing composer dependencies...\nRestarting FPM failed...");
 
     $this->artisan('deploy', ['site' => 2]);
 })->throws('The deployment failed.');
