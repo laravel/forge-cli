3480016dc7d36a710299358496a7808517d37c5a
Josh Salway <josh.salway@gmail.com>
2026-04-07 10:26:50 +1000

    Update logout test for confirmation prompt

--- tests/Feature/LogoutCommandTest.php
+++ tests/Feature/LogoutCommandTest.php
@@ -2,6 +2,7 @@
 
 it('logout users', function () {
     $this->artisan('logout')
+        ->expectsQuestion('<fg=yellow>‣</> <options=bold>Are You Sure You Want To Log Out? This Will Remove Your Stored API Token And Configuration</>', 'yes')
         ->expectsOutput('==> Logged Out Successfully');
 
     expect($this->config->get('server'))->toBeNull();
